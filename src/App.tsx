716e58ce0d0ee5d2cf3f2b058200fa0988dc7bd8
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-10-31 17:03:53 +0200

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter} from 'react-router-dom';
+import AppRouter from './components/app-router/AppRouter';
+import ErrorSnackbar from './components/common/ErrorSnackbar/ErrorSnackbar';
+import {Loader} from './components/common/Loader/Loader';
+import {useAppDispatch, useAppSelector} from './bll/store/store';
+import {authMe} from './bll/reducers/app-reducer';
+import {ModalWindow} from "./components/modal-window/ModalWindow";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useAppDispatch()
+
+    const isInitialize = useAppSelector<boolean>(state => state.appReducer.isInitialized)
+
+    React.useEffect(() => {
+        dispatch(authMe())
+    }, [])
+
+    if (!isInitialize) return <Loader/>
+
+    return (
+        <>
+            <HashRouter>
+                <ModalWindow/>
+                <ErrorSnackbar/>
+                <AppRouter/>
+            </HashRouter>
+        </>
+
+    );
 }
 
 export default App;
